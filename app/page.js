2e899997ca0376634ca0db25dc82a700286ccf46
Carlos Rojas Vega <karlosrvxx@gmail.com>
2023-06-26 12:19:49 -0500

    modificado logo por nombre

--- app/page.js
+++ app/page.js
@@ -29,14 +29,8 @@ export default function Home() {
       </div>
 
       <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+        <h1>Carlos Rojas</h1>
+
       </div>
 
       <div className={styles.grid}>
